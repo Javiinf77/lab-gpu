f0b420ea619c38500a59f3c1459790fa91f01388
javiinf77 <javism1d@gmail.com>
2024-12-16 13:40:02 +0100

    Lab GPU: 2a parte

--- submit_reduc_oper-GPU_hpc-bio-nikola-gpu-alumno21.sh
+++ submit_reduc_oper-GPU_hpc-bio-nikola-gpu-alumno21.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=reduc_gpu
-#SBATCH --output=reduc_gpu_%j.out
-#SBATCH --error=reduc_gpu_%j.err
+#SBATCH --output=reduc_gpu_%j-5*10^8.out
+#SBATCH --error=reduc_gpu_%j-5*10^8.err
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --partition=hpc-bio-nikola-gpu
 #SBATCH --time=01:00:00
 
 module load anaconda/2023.03
-ipython reduc-operation-array_gpu-alumno21.ipynb -- --value=5000000
+ipython reduc-operation-array_gpu-alumno21.ipynb -- --value=500000000
 
